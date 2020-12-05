15c10533dbe07643f6e87e0101efbcd6bb2a6266
Pablo Acuna <pacuna@pm.me>
2020-12-04 23:54:54 -0600

    add day4 partI

---
+++ day04/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(f)
+	var valids int
+	var line string
+	for scanner.Scan() {
+		if len(scanner.Text()) == 0 {
+			if isValid(line) {
+				valids++
+			}
+			line = ""
+		} else {
+			line += " "
+			line += scanner.Text()
+		}
+	}
+	if isValid(line) {
+		valids++
+	}
+	fmt.Println(valids)
+
+}
+
+func isValid(data string) bool {
+	validFields := map[string]bool{
+		"byr": true,
+		"iyr": true,
+		"eyr": true,
+		"hgt": true,
+		"hcl": true,
+		"ecl": true,
+		"pid": true,
+		"cid": true,
+	}
+	for _, field := range strings.Fields(data) {
+		fieldName := strings.Split(field, ":")[0]
+		if _, ok := validFields[fieldName]; ok {
+			delete(validFields, fieldName)
+		}
+	}
+	return len(validFields) == 0 || (len(validFields) == 1 && validFields["cid"])
+}
