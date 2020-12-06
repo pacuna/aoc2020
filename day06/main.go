a765dc8d195b27000314f411df4025f3e3d264b6
Pablo Acuna <pacuna@pm.me>
2020-12-06 13:29:58 -0600

    add day 6 part II

--- day06/main.go
+++ day06/main.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-func solve(filename string) (int, error) {
+func solveI(filename string) (int, error) {
 	f, err := os.Open(filename)
 	if err != nil {
 		return -1, err
@@ -39,6 +39,53 @@ func solve(filename string) (int, error) {
 
 	return res, nil
 }
+
+func solveII(filename string) (int, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return -1, err
+	}
+	scanner := bufio.NewScanner(f)
+	var line string
+	curGroup := make(map[rune]int)
+	var res int
+	var numPeople int
+	for scanner.Scan() {
+		if len(scanner.Text()) == 0 {
+			// process line
+			for _, c := range line {
+				curGroup[c]++
+			}
+
+			for _, v := range curGroup {
+				if v == numPeople {
+					res++
+				}
+			}
+
+			// reset
+			line = ""
+			curGroup = make(map[rune]int)
+			numPeople = 0
+		} else {
+			line += scanner.Text()
+			numPeople++
+		}
+	}
+
+	// process last line
+	for _, c := range line {
+		curGroup[c]++
+	}
+	for _, v := range curGroup {
+		if v == numPeople {
+			res++
+		}
+	}
+
+	return res, nil
+}
 func main() {
-	fmt.Println(solve("input.txt"))
+	fmt.Println(solveI("input.txt"))
+	fmt.Println(solveII("input.txt"))
 }
--- day06/main_test.go
+++ day06/main_test.go
@@ -6,8 +6,14 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestSolve(t *testing.T) {
-	res, err := solve("example.txt")
+func TestSolveI(t *testing.T) {
+	res, err := solveI("example.txt")
 	require.NoError(t, err)
 	require.Equal(t, 11, res)
+}
+
+func TestSolveII(t *testing.T) {
+	res, err := solveII("example.txt")
+	require.NoError(t, err)
+	require.Equal(t, 6, res)
 }
