b593dd08cc91f16feee130e544b9da196ff03d5b
Pablo Acuna <pacuna@pm.me>
2020-12-10 02:03:37 +0000

    add day08 part II

--- day08/main.go
+++ day08/main.go
@@ -12,6 +12,57 @@ type Instruction struct {
 	val int
 }
 
+// return true if loops and last value of accumulator
+func loops(instructions []Instruction) (bool, int) {
+	var res int
+	var cur Instruction
+
+	var i int
+	seen := make(map[int]int)
+	for {
+		if i == len(instructions) {
+			return false, res
+		}
+		cur = instructions[i]
+		seen[i]++
+		if seen[i] == 2 {
+			return true, res
+		}
+		if cur.cmd == "nop" {
+			i++
+		} else if cur.cmd == "acc" {
+			res += cur.val
+			i++
+		} else if cur.cmd == "jmp" {
+			i += cur.val
+		}
+
+	}
+	return false, res
+}
+
+func swap(instructions []Instruction) int {
+	var res int
+	for idx, ins := range instructions {
+		if ins.cmd == "nop" {
+			instructions[idx].cmd = "jmp"
+			if l, acc := loops(instructions); !l {
+				return acc
+			} else {
+				instructions[idx].cmd = "nop"
+			}
+		} else if ins.cmd == "jmp" {
+			instructions[idx].cmd = "nop"
+			if l, acc := loops(instructions); !l {
+				return acc
+			} else {
+				instructions[idx].cmd = "jmp"
+			}
+		}
+	}
+	return res
+}
+
 func main() {
 	f, err := os.Open("input.txt")
 	if err != nil {
@@ -25,26 +76,8 @@ func main() {
 		instructions = append(instructions, ins)
 	}
 
-	var res int
-	var cur Instruction
-
-	var i int
-	seen := make(map[int]int)
-	for {
-		cur = instructions[i]
-		seen[i]++
-		if seen[i] == 2 {
-			fmt.Printf("breaking with acc: %d\n", res)
-			break
-		}
-		if cur.cmd == "nop" {
-			i++
-		} else if cur.cmd == "acc" {
-			res += cur.val
-			i++
-		} else if cur.cmd == "jmp" {
-			i += cur.val
-		}
-
-	}
+	// part I:
+	fmt.Println(loops(instructions))
+	// part II:
+	fmt.Println(swap(instructions))
 }
